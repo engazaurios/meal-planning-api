1951e23d93f5287ade3c63d4e10a3c19378a658a
Henzer Garcia <hgarca@xoom.com>
2019-07-01 17:15:14 -0600

    [permissions] Remove unnecesary changes

--- common/models/user-menu.js
+++ common/models/user-menu.js
@@ -1,17 +1,5 @@
 'use strict';
 
 module.exports = function(UserMenu) {
-  UserMenu.getMenuPerDate = function(callback) {
 
-  };
-
-  UserMenu.remoteMethod('menu-per-date', {
-    http: {
-      path: '/menus-per-date',
-      verb: 'get',
-    },
-    returns: {
-      
-    },
-  });
 };
