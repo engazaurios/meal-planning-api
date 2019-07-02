f8ee9bf16afa5be0581c5af2f2228b7b7933a0fe
Henzer Garcia <hgarca@xoom.com>
2019-07-01 19:52:23 -0600

    [add-menus-per-date-method] Add getMenusCombined

--- common/models/day-menu.js
+++ common/models/day-menu.js
@@ -1,5 +1,40 @@
 'use strict';
 
-module.exports = function(Daymenu) {
+module.exports = function(DayMenu) {
+  DayMenu.getMenusPerDate = function(startDate, endDate, callback) {
+    DayMenu.find({
+      where: {and: [
+        {date: {gte: startDate}},
+        {date: {lte: endDate}},
+      ]},
+    }, function(err, menus) {
+      if (err) {
+        throw err;
+      }
+      callback(null, menus);
+    });
+  };
 
+  DayMenu.remoteMethod('getMenusPerDate', {
+    http: {
+      path: '/menus-per-date/:startDate/:endDate',
+      verb: 'get',
+    },
+    accepts: [
+      {
+        arg: 'startDate',
+        type: 'date',
+        required: true,
+      },
+      {
+        arg: 'endDate',
+        type: 'date',
+        required: true,
+      },
+    ],
+    returns: {
+      arg: 'menus',
+      type: 'array',
+    },
+  });
 };
--- common/models/user-menu.js
+++ common/models/user-menu.js
@@ -2,8 +2,6 @@
 
 module.exports = function(UserMenu) {
   UserMenu.getMenusPerDate = function(userId, startDate, endDate, callback) {
-    console.log(startDate);
-    console.log(endDate);
     UserMenu.find({
       where: {and: [
         {date: {gte: startDate}},
@@ -18,9 +16,47 @@ module.exports = function(UserMenu) {
     });
   };
 
+  UserMenu.getMenusPerDateCombined = function(userId, startDate, endDate, callback) {
+    UserMenu.getMenusPerDate(userId, startDate, endDate, function(err, userMenus) {
+      var DayMenu = UserMenu.app.models.DayMenu;
+      DayMenu.getMenusPerDate(startDate, endDate, function(err, dayMenus) {
+        var menus = userMenus.concat(dayMenus);
+        callback(null, menus);
+      });
+    });
+  };
+
   UserMenu.remoteMethod('getMenusPerDate', {
     http: {
       path: '/menus-per-date/:userId/:startDate/:endDate',
+      verb: 'get',
+    },
+    accepts: [
+      {
+        arg: 'userId',
+        type: 'string',
+        required: true,
+      },
+      {
+        arg: 'startDate',
+        type: 'date',
+        required: true,
+      },
+      {
+        arg: 'endDate',
+        type: 'date',
+        required: true,
+      },
+    ],
+    returns: {
+      arg: 'menus',
+      type: 'array',
+    },
+  });
+
+  UserMenu.remoteMethod('getMenusPerDateCombined', {
+    http: {
+      path: '/menus-per-date-combined/:userId/:startDate/:endDate',
       verb: 'get',
     },
     accepts: [
