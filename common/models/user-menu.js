52bceb0f0005bc37edd126990894cfa758d41c2d
Henzer Garcia <hgarca@xoom.com>
2019-07-01 18:51:32 -0600

    [add-menus-per-date-method] Add remote method

--- common/models/user-menu.js
+++ common/models/user-menu.js
@@ -1,5 +1,48 @@
 'use strict';
 
 module.exports = function(UserMenu) {
+  UserMenu.getMenusPerDate = function(userId, startDate, endDate, callback) {
+    console.log(startDate);
+    console.log(endDate);
+    UserMenu.find({
+      where: {and: [
+        {date: {gte: startDate}},
+        {date: {lte: endDate}},
+        {userId: userId},
+      ]},
+    }, function(err, menus) {
+      if (err) {
+        throw err;
+      }
+      callback(null, menus);
+    });
+  };
 
+  UserMenu.remoteMethod('getMenusPerDate', {
+    http: {
+      path: '/menus-per-date/:userId/:startDate/:endDate',
+      verb: 'get',
+    },
+    accepts: [
+      {
+        arg: 'userId',
+        type: 'string',
+        required: true,
+      },
+      {
+        arg: 'startDate',
+        type: 'date',
+        required: true,
+      },
+      {
+        arg: 'endDate',
+        type: 'date',
+        required: true,
+      },
+    ],
+    returns: {
+      arg: 'menus',
+      type: 'array',
+    },
+  });
 };
