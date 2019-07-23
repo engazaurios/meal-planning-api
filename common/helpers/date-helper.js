7c2a52d29ba2043cef8ece820568b44804d9036b
Henzer Garcia <hgarca@xoom.com>
2019-07-22 22:54:03 -0600

    [add-menus-per-date-method] Use time as UTC in the backend

--- common/helpers/date-helper.js
+++ common/helpers/date-helper.js
@@ -4,18 +4,18 @@
 var currentWeekNumber = require('current-week-number');
 
 const getDateWithoutTime = (date) => {
-  return date.getUTCFullYear() + '-' +
-  (date.getUTCMonth() + 1) + '-' +
-  date.getUTCDate();
+  return date.getFullYear() + '-' +
+  (date.getMonth() + 1) + '-' +
+  date.getDate();
 };
 
 const getDayInfo = (day) => {
   return {
     'date': getDateWithoutTime(day),
     'dateId': new Date(getDateWithoutTime(day)),
-    'day': day.getUTCDate(),
-    'month': day.getUTCMonth() + 1,
-    'year': day.getUTCFullYear(),
+    'day': day.getDate(),
+    'month': day.getMonth() + 1,
+    'year': day.getFullYear(),
     'week': currentWeekNumber(day),
     'weekend': (day.getDay() == 0 || day.getDay() == 6),
   };
@@ -31,25 +31,6 @@ const getDates = (startDate, endDate) => {
   return dates;
 };
 
-const getDatesPerWeekNumber = (year, week) => {
-  var dates = [];
-  var day = new Date(year, 0, 1);
-  while (day.getUTCFullYear() == year) {
-    var dayInfo = getDayInfo(day);
-    if (dayInfo.week == week) {
-      for (var i = 0; i < 7; i++) {
-        dates.push(dayInfo);
-        day.setDate(day.getDate() + 1);
-        dayInfo = getDayInfo(day);
-      }
-      console.log(dates);
-      return dates;
-    }
-    day.setDate(day.getDate() + 1);
-  }
-  return dates;
-};
-
 const getWholeWeek = (startDate) => {
   const dates = [];
   for (var i = 0; i < 7; i++) {
@@ -61,7 +42,6 @@ const getWholeWeek = (startDate) => {
 
 module.exports = {
   getDates,
-  getDatesPerWeekNumber,
   getDateWithoutTime,
   getWholeWeek,
 };
