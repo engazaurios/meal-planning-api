557bd9a437ccca5fae3beb588bb0f0a55de810a4
Cesar Luis <cesarluis13915@gmail.com>
2019-09-06 20:33:22 -0600

    Implement version 2 of report (one tab per meal time).

--- server/boot/report.js
+++ server/boot/report.js
@@ -17,11 +17,10 @@ module.exports = function(app) {
 
 
   app.get('/api/get-report', function(req, res, next) {
-    // Ref: http://www.ihamvic.com/2018/07/25/create-and-download-excel-file-in-node-js/
+    var unifiedReport = req.query.unified === 'true';
     var workbook = new excel.Workbook();
-    var sheet = workbook.addWorksheet('Report');
 
-    var objArray =[
+    var objArray = [
       {
         user: {
           name: 'James',
@@ -832,17 +831,13 @@ module.exports = function(app) {
       },
     ];
 
-    sheet.views = [
-      { state: 'frozen', xSplit: 1, ySplit: 3 }
-    ];
 
+    function printReportHeader(sheet, dates, lunchTime) {
+      lunchTime = ['breakfast', 'lunch', 'dinner'].includes(lunchTime) ? lunchTime : null;
 
-
-
-    function printReportHeader(sheet, dates) {
       sheet.addRow(['Empleado', 'Departamento']);
-      sheet.mergeCells(1, 1, 3, 1);
-      sheet.mergeCells(1, 2, 3, 2);
+      sheet.mergeCells(1, 1, lunchTime ? 2 : 3, 1);
+      sheet.mergeCells(1, 2, lunchTime ? 2 : 3, 2);
 
       excelUtils.applyVerticalAligmentMiddleToCell(sheet.getCell('A1'));
       excelUtils.applyVerticalAligmentMiddleToCell(sheet.getCell('B1'));
@@ -862,12 +857,11 @@ module.exports = function(app) {
 
       // --------- PRINT DAYS ---------
       var col, cell;
+      var datesRow = sheet.getRow(1);
 
       col = 3;
-      var firstRow = sheet.getRow(1);
-
       dates.forEach(function(day) {
-        cell = firstRow.getCell(col);
+        cell = datesRow.getCell(col);
 
         cell.value = day;
 
@@ -875,54 +869,56 @@ module.exports = function(app) {
         excelUtils.applyBoldToCell(cell);
         excelUtils.applyAllBordersToCell(cell);
 
-        sheet.mergeCells(1, col, 1, col + 8);
+        sheet.mergeCells(1, col, 1, lunchTime ? col + 2 : col + 8);
 
-        col += 9;
+        col += lunchTime ? 3 : 9;
       });
 
       // --------- PRINT LUCNH TIMES ---------
-      col = 3;
-      var secondRow = sheet.getRow(2);
+      if (!lunchTime) {
+        var lunchTimesRow = sheet.getRow(2);
 
-      dates.forEach(function(day) {
-        cell = secondRow.getCell(col);
+        col = 3;
+        dates.forEach(function(day) {
+          cell = lunchTimesRow.getCell(col);
 
-        cell.value = 'Desayuno';
-        excelUtils.applyHorizontalAligmentCenterToCell(cell);
-        excelUtils.applyBoldToCell(cell);
-        excelUtils.applyAllBordersToCell(cell);
+          cell.value = 'Desayuno';
+          excelUtils.applyHorizontalAligmentCenterToCell(cell);
+          excelUtils.applyBoldToCell(cell);
+          excelUtils.applyAllBordersToCell(cell);
 
-        sheet.mergeCells(2, col, 2, col + 2);
-        col += 3;
+          sheet.mergeCells(2, col, 2, col + 2);
+          col += 3;
 
-        cell = secondRow.getCell(col);
+          cell = lunchTimesRow.getCell(col);
 
-        cell.value = 'Almuerzo';
-        excelUtils.applyHorizontalAligmentCenterToCell(cell);
-        excelUtils.applyBoldToCell(cell);
-        excelUtils.applyAllBordersToCell(cell);
+          cell.value = 'Almuerzo';
+          excelUtils.applyHorizontalAligmentCenterToCell(cell);
+          excelUtils.applyBoldToCell(cell);
+          excelUtils.applyAllBordersToCell(cell);
 
-        sheet.mergeCells(2, col, 2, col + 2);
-        col += 3;
+          sheet.mergeCells(2, col, 2, col + 2);
+          col += 3;
 
-        cell = secondRow.getCell(col);
+          cell = lunchTimesRow.getCell(col);
 
-        cell.value = 'Cena';
-        excelUtils.applyHorizontalAligmentCenterToCell(cell);
-        excelUtils.applyBoldToCell(cell);
-        excelUtils.applyAllBordersToCell(cell);
+          cell.value = 'Cena';
+          excelUtils.applyHorizontalAligmentCenterToCell(cell);
+          excelUtils.applyBoldToCell(cell);
+          excelUtils.applyAllBordersToCell(cell);
 
-        sheet.mergeCells(2, col, 2, col + 2);
-        col += 3;
-      });
+          sheet.mergeCells(2, col, 2, col + 2);
+          col += 3;
+        });
+      }
 
       // --------- PRINT MENUS ---------
-      col = 3;
-      var thirdRow = sheet.getRow(3);
+      var menusRow = sheet.getRow(lunchTime ? 2 : 3);
 
+      col = 3;
       dates.forEach(function(day) {
-        for (var i = 0; i < 3; i++) {
-          cell = thirdRow.getCell(col);
+        for (var i = 0; i < (lunchTime ? 1 : 3); i++) {
+          cell = menusRow.getCell(col);
           cell.value = 'Menu';
           excelUtils.applyHorizontalAligmentCenterToCell(cell);
           excelUtils.applyBoldToCell(cell);
@@ -931,7 +927,7 @@ module.exports = function(app) {
 
           col += 1;
 
-          cell = thirdRow.getCell(col);
+          cell = menusRow.getCell(col);
           cell.value = 'Estado';
           excelUtils.applyHorizontalAligmentCenterToCell(cell);
           excelUtils.applyBoldToCell(cell);
@@ -939,7 +935,7 @@ module.exports = function(app) {
 
           col += 1;
 
-          cell = thirdRow.getCell(col);
+          cell = menusRow.getCell(col);
           cell.value = 'Valor';
           excelUtils.applyHorizontalAligmentCenterToCell(cell);
           excelUtils.applyBoldToCell(cell);
@@ -950,16 +946,20 @@ module.exports = function(app) {
       });
     }
 
-    function printReportData(sheet, data, dates) {
+    function printReportData(sheet, data, dates, lunchTime) {
       data = Array.isArray(data) && data.length ? data : [];
       dates = Array.isArray(dates) && dates.length ? dates : [];
+      lunchTime = ['breakfast', 'lunch', 'dinner'].includes(lunchTime) ? lunchTime : null;
 
       var col;
       var row, cell;
       var userDayMeals;
+      var multiplier = lunchTime ? 3 : 9;
+      var startRow = lunchTime ? 3 : 4;
 
+      // --------- PRINT DATA ---------
       data.forEach(function(userData, index) {
-        row = sheet.getRow(4 + index);
+        row = sheet.getRow(startRow + index);
         col = 1;
 
         cell = row.getCell(col);
@@ -976,59 +976,64 @@ module.exports = function(app) {
           });
 
           if (!userDayMeals) {
-            col += 9;
+            col += multiplier;
 
             return;
           }
 
           // Breakfast
-          if ('breakfast' in userDayMeals.meals) {
-            row.getCell(col).value = userDayMeals.meals.breakfast.menu.name;
-            col += 1;
-            row.getCell(col).value = userDayMeals.meals.breakfast.status;
-            col += 1;
-            row.getCell(col).value = userDayMeals.meals.breakfast.cost;
-            col += 1;
-          } else {
-            col += 3;
+          if (!lunchTime || (lunchTime === 'breakfast')) {
+            if (userDayMeals.meals.breakfast) {
+              row.getCell(col).value = userDayMeals.meals.breakfast.menu.name;
+              col += 1;
+              row.getCell(col).value = userDayMeals.meals.breakfast.status;
+              col += 1;
+              row.getCell(col).value = userDayMeals.meals.breakfast.cost;
+              col += 1;
+            } else {
+              col += 3;
+            }
           }
 
           // Lunch
-          if ('lunch' in userDayMeals.meals) {
-            row.getCell(col).value = userDayMeals.meals.lunch.menu.name;
-            col += 1;
-            row.getCell(col).value = userDayMeals.meals.lunch.status;
-            col += 1;
-            row.getCell(col).value = userDayMeals.meals.lunch.cost;
-            col += 1;
-          } else {
-            col += 3;
+          if (!lunchTime || (lunchTime === 'lunch')) {
+            if (userDayMeals.meals.lunch) {
+              row.getCell(col).value = userDayMeals.meals.lunch.menu.name;
+              col += 1;
+              row.getCell(col).value = userDayMeals.meals.lunch.status;
+              col += 1;
+              row.getCell(col).value = userDayMeals.meals.lunch.cost;
+              col += 1;
+            } else {
+              col += 3;
+            }
           }
 
           // Lunch
-          if ('dinner' in userDayMeals.meals) {
-            row.getCell(col).value = userDayMeals.meals.dinner.menu.name;
-            col += 1;
-            row.getCell(col).value = userDayMeals.meals.dinner.status;
-            col += 1;
-            row.getCell(col).value = userDayMeals.meals.dinner.cost;
-            col += 1;
-          } else {
-            col += 3;
+          if (!lunchTime || (lunchTime === 'dinner')) {
+            if (userDayMeals.meals.dinner) {
+              row.getCell(col).value = userDayMeals.meals.dinner.menu.name;
+              col += 1;
+              row.getCell(col).value = userDayMeals.meals.dinner.status;
+              col += 1;
+              row.getCell(col).value = userDayMeals.meals.dinner.cost;
+              col += 1;
+            } else {
+              col += 3;
+            }
           }
         });
       });
 
-      // TOTALS
-      var lastRow = 5 + data.length;
-
-      row = sheet.getRow(lastRow);
-
-      cell = row.getCell(1);
-      cell.value = 'TOTALES';
-
+      // --------- PRINT TOTALS ---------
       var breakfastTotal, lunchTotal, dinnerTotal;
       var grandTotal = 0;
+      var lastRow = (lunchTime ? 4 : 5) + data.length;
+
+      totalsRow = sheet.getRow(lastRow);
+
+      cell = totalsRow.getCell(1);
+      cell.value = 'TOTALES';
 
       col = 5;
       dates.forEach(function(date) {
@@ -1071,36 +1076,51 @@ module.exports = function(app) {
           return sum;
         }, 0);
 
-        grandTotal += breakfastTotal + lunchTotal + dinnerTotal;
 
-        cell = row.getCell(col);
-        cell.value = breakfastTotal;
-        col += 3;
+        if (!lunchTime || (lunchTime === 'breakfast')) {
+          cell = totalsRow.getCell(col);
+          cell.value = breakfastTotal;
+          col += 3;
 
-        cell = row.getCell(col);
-        cell.value = lunchTotal;
-        col += 3;
+          grandTotal += breakfastTotal;
+        }
 
-        cell = row.getCell(col);
-        cell.value = dinnerTotal;
-        col += 3;
+        if (!lunchTime || (lunchTime === 'lunch')) {
+          cell = totalsRow.getCell(col);
+          cell.value = lunchTotal;
+          col += 3;
+
+          grandTotal += lunchTotal;
+        }
+
+        if (!lunchTime || (lunchTime === 'dinner')) {
+          cell = totalsRow.getCell(col);
+          cell.value = dinnerTotal;
+          col += 3;
+
+          grandTotal += dinnerTotal;
+        }
       });
 
-      for (var i = 1; i <= 4 + dates.length * 9; i++) {
-        cell = row.getCell(i);
+      // Apply border and bold to totals row.
+      for (var i = 1; i <= 4 + dates.length * multiplier; i++) {
+        cell = totalsRow.getCell(i);
 
         excelUtils.applyBordersToCell(cell, { top: true, bottom: true }, 'double');
         excelUtils.applyBoldToCell(cell);
       }
 
-      cell = row.getCell(3 + dates.length * 9);
+      // Print grand total
+      cell = totalsRow.getCell(3 + dates.length * multiplier);
       cell.value = grandTotal;
       excelUtils.applyBordersToCell(cell, { left: true, top: true, bottom: true }, 'double');
 
       // Merge grand total cells.
-      sheet.mergeCells(lastRow, 3 + dates.length * 9, lastRow, 4 + dates.length * 9);
+      sheet.mergeCells(lastRow, 3 + dates.length * multiplier, lastRow, 4 + dates.length * multiplier);
     }
 
+
+    var sheet;
     var dates = objArray[0].meals.reduce(function(dates, userDayData) {
       if (!dates.includes(userDayData.day)) {
         dates.push(userDayData.day);
@@ -1109,11 +1129,43 @@ module.exports = function(app) {
       return dates;
     }, []);
 
-    printReportHeader(sheet, dates);
-    printReportData(sheet, objArray, dates);
+    if (unifiedReport) {
+      sheet = workbook.addWorksheet('Report');
+      sheet.views = [
+        { state: 'frozen', xSplit: 1, ySplit: 3 }
+      ];
+
+      printReportHeader(sheet, dates);
+      printReportData(sheet, objArray, dates);
+    } else {
+      sheet = workbook.addWorksheet('Desayuno');
+      sheet.views = [
+        { state: 'frozen', xSplit: 1, ySplit: 2 }
+      ];
+
+      printReportHeader(sheet, dates, 'breakfast');
+      printReportData(sheet, objArray, dates, 'breakfast');
+
+      sheet = workbook.addWorksheet('Almuerzo');
+      sheet.views = [
+        { state: 'frozen', xSplit: 1, ySplit: 2 }
+      ];
+
+      printReportHeader(sheet, dates, 'lunch');
+      printReportData(sheet, objArray, dates, 'lunch');
+
+      sheet = workbook.addWorksheet('Cena');
+      sheet.views = [
+        { state: 'frozen', xSplit: 1, ySplit: 2 }
+      ];
+
+      printReportHeader(sheet, dates, 'dinner');
+      printReportData(sheet, objArray, dates, 'dinner');
+    }
 
     var tempFilePath = tempfile('.xlsx');
 
+    // Ref: http://www.ihamvic.com/2018/07/25/create-and-download-excel-file-in-node-js/
     workbook.xlsx.writeFile(tempFilePath).then(function() {
       var resOptions = {
         headers: {
