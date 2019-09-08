48e8937561af8535812494be21e0cd7fa5a33d82
Henzer Garcia <henzer.g.09@gmail.com>
2019-09-08 12:05:19 -0600

    Add the Report method on UserMenu model

--- common/helpers/data-provider.js
+++ common/helpers/data-provider.js
@@ -2,25 +2,28 @@
 'use strict';
 
 const getData = (models, startDate, endDate, costCenters, users) => {
-    const UserMenu = models.UserMenu;
-    const AppUser = models.AppUser;
+  const UserMenu = models.UserMenu;
+  const AppUser = models.AppUser;
 
-    return UserMenu.find({
-      where: {and: [
-        {date: {gte: startDate}},
-        {date: {lte: endDate}},
-      ]},
-    }).then(userMenus => {
-      const usersQuery = userMenus.map(userMenu => {
-        return AppUser.findOne({
-          where: {_id: userMenu.userId}
-        }).then(user => {
-          userMenu.user = user;
-          return userMenu;
-        });
-      });
-      return Promise.all(usersQuery);
-    });
+  return UserMenu.find({
+    include: 'user',
+    where: {and: [
+      {date: {gte: startDate}},
+      {date: {lte: endDate}},
+      {userId: (users) ? {inq: users} : {nin: []}}
+    ]},
+  }).then(userMenus => {
+    // Filter the included relation
+    return userMenus.filter(userMenu => {
+      if (!userMenu.user() || !userMenu.user().costCenter()) {
+        return false;
+      }
+      if (costCenters && !costCenters.includes(userMenu.user().costCenterId.toString())) {
+        return false;
+      }
+      return true;
+    })
+  });
 }
 
 module.exports = {
